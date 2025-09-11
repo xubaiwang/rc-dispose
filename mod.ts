ee8e25c63e2a9d66a7cf6957762007fcb98d2a11
Xubai Wang <18016038327@189.cn>
2025-09-11 13:15:05 +0800

    docs: add jsdoc

--- mod.ts
+++ mod.ts
@@ -1,15 +1,47 @@
+/**
+ * Wrap a disposable or async disposable value in RC,
+ * so that it have to be disposed multiple times to actually dispose it.
+ *
+ * @param value the value to wrap
+ * @param options extra options, see {@link RcOptionsSimple}
+ * @returns reference counted disposable or async disposable
+ */
 function rc<T extends Disposable | AsyncDisposable>(
   value: T,
   options?: RcOptionsSimple,
 ): T;
+/**
+ * Wrap a disposable or async disposable value in RC,
+ * so that it have to be disposed multiple times to actually dispose it.
+ *
+ * @param value the value to wrap
+ * @param options extra options, see {@link RcOptionsDispose}
+ * @returns reference counted disposable or async disposable, with dispose overridden
+ */
 function rc<T extends object>(
   value: T,
   options?: RcOptionsDispose,
 ): T & Disposable;
+/**
+ * Wrap a disposable or async disposable value in RC,
+ * so that it have to be disposed multiple times to actually dispose it.
+ *
+ * @param value the value to wrap
+ * @param options extra options, see {@link RcOptionsDispose}
+ * @returns reference counted disposable or async disposable, with asyncDispose overridden
+ */
 function rc<T extends object>(
   value: T,
   options?: RcOptionsAsyncDispose,
 ): T & AsyncDisposable;
+/**
+ * Wrap a disposable or async disposable value in RC,
+ * so that it have to be disposed multiple times to actually dispose it.
+ *
+ * @param value the value to wrap
+ * @param options extra options, see {@link RcOptionsDispose}
+ * @returns reference counted disposable or async disposable, with both dispose and asyncDispose overridden
+ */
 function rc<T extends object>(
   value: T,
   options?: RcOptionsDispose & RcOptionsAsyncDispose,
@@ -76,21 +108,38 @@ function rc<T extends object>(
 
 export { rc };
 
+/**
+ * Extra options of {@link rc}.
+ */
 export type RcOptions =
   | RcOptionsSimple
   | RcOptionsDispose
   | RcOptionsAsyncDispose;
 
+/**
+ * Extra options of {@link rc}, simple options.
+ */
 export interface RcOptionsSimple {
+  /** The initial count of rc. */
   count?: number;
 }
 
+/**
+ * Extra options of {@link rc}, with dispose override.
+ */
 export interface RcOptionsDispose {
+  /** The initial count of rc. */
   count?: number;
+  /** Use custom `dispose` instead of the value's `Symbol.dispose`. */
   dispose?(): void;
 }
 
+/**
+ * Extra options of {@link rc}, with async dispose override.
+ */
 export interface RcOptionsAsyncDispose {
+  /** The initial count of rc. */
   count?: number;
+  /** Use custom `asyncDispose` instead of the value's `Symbol.asyncDispose`. */
   asyncDispose?(): Promise<void>;
 }
