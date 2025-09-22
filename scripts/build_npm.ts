d81121f371ee203d3fd67111024053e4ce03ffbf
Xubai Wang <18016038327@189.cn>
2025-09-22 15:36:24 +0800

    chore: dnt npm

---
+++ scripts/build_npm.ts
@@ -0,0 +1,35 @@
+import { build, emptyDir } from "jsr:@deno/dnt";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  compilerOptions: {
+    lib: ["ESNext"],
+  },
+  package: {
+    // package.json properties
+    name: "rc-dispose",
+    version: Deno.args[0],
+    description:
+      "Reference counted explicit resource management in TypeScript.",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/xubaiwang/rc-dispose.git",
+    },
+    bugs: {
+      url: "https://github.com/xubaiwang/rc-dispose/issues",
+    },
+  },
+  postBuild() {
+    // steps to run after building and before running the tests
+    Deno.copyFileSync("LICENSE", "npm/LICENSE");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});
